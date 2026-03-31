5e15f237fedd90dca6638863743614c30de2d8e1
sofia <sofiagracia000@gmail.com>
2026-03-31 11:28:32 +0200

    refactor(#11): refactor name dialog components

--- src/app/projects/components/projects/projects.ts
+++ src/app/projects/components/projects/projects.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ProjectCard } from '@projects/components/project-card/project-card';
-import { DialogCMProject } from '../dialog-cm-project/dialog-cm-project';
+import { DialogProject } from '../dialog-project/dialog-project';
 import { ProjectsFacade } from '@projects/facades/projects.facade';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'app-projects',
-    imports: [ProjectCard, DialogCMProject, AsyncPipe],
+    imports: [ProjectCard, DialogProject, AsyncPipe],
     templateUrl: './projects.html',
 })
 export class Projects {

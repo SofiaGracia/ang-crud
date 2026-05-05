c442d67a023e4b9f6cd931fbf58290eb4fc48c20
sofia <sofiagracia000@gmail.com>
2026-05-05 18:48:57 +0200

    feat(#30): Added environment.development.ts

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    baseUrl: 'http://localhost:3000',
+
+    supabaseUrl: 'https://xjcdphpsoohbuftyozag.supabase.co',
+
+    supabaseKey: 'sb_publishable_bDVHTRbwjOXhWGKQDtcQ-A_PaYmDdzw'
+};
